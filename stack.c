0f97a3932f211267584cea4d8e2ac9763cdae55a
Owen <ooliveira9359@gmail.com>
2024-12-15 23:43:08 -0500

    initialized file, planned stack

---
+++ stack.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// push
+// pop
+// top: returns top element of the stack
+// isEmpty:returns true if stack is empty else false
+// isFull: returns true if the stack is full else false
+    // if(top == capacity-1) stack overflows
+    // else (top = top+1)
