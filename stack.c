90e559436235cc0c37a122d7c841b647ff1a062d
Owen <ooliveira9359@gmail.com>
2024-12-17 22:40:16 -0500

    added push function for stack

--- stack.c
+++ stack.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// max capacity of stack
+#define SIZE 4
+
+int top = -1;
+int inp_array[SIZE];
+void push();
+void pop();
+void show();
+
+int main (){
+
+}
+
+
+void push(){
+    // stores variable to be pushed onto the stack
+    int x;
+    
+    // top represents index of current top of stack
+    if (top == SIZE -1){
+        printf("\nOverflow!\n");
+    // if there is room, ask user for what to add
+    } else{
+        printf("\nEnter elements to add: ");
+        scanf("%d", &x);
+        // increment top to add 1 for the element added
+        top = top + 1;
+        //store new element in array loop indexed by top
+        inp_array[top] = x;
+    }
+}
 // push
 // pop
 // top: returns top element of the stack
