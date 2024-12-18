2f4c2cf4e19dabdbb48b6c8519896bb709140ddb
Owen <ooliveira9359@gmail.com>
2024-12-17 23:11:57 -0500

    added main function and fixed bugs

--- stack.c
+++ stack.c
@@ -1,41 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// max capacity of stack
+// Max capacity of stack
 #define SIZE 4
 
-int top = -1;
-int inp_array[SIZE];
+int top = -1; // Initialize top to -1 (empty stack)
+int inp_array[SIZE]; // Array to hold stack elements
+
+// Stack functions
 void push();
 void pop();
 void show();
 
-int main (){
+int main() {
+    int choice;
 
-}
+    while (1) {
+        printf("\n\nPracticing stack functions:");
+        printf("\nPress 1 to push the element");
+        printf("\nPress 2 to pop element");
+        printf("\nPress 3 to show elements");
+        printf("\nPress 4 to exit");
+        printf("\n\nEnter choice: ");
 
+        // Input validation for choice
+        if (scanf("%d", &choice) != 1) {
+            printf("\nInvalid input! Please enter a valid number.");
+            while (getchar() != '\n'); // Clear the input buffer
+            continue;
+        }
 
-void push(){
-    // stores variable to be pushed onto the stack
-    int x;
-    
-    // top represents index of current top of stack
-    if (top == SIZE -1){
-        printf("\nOverflow!\n");
-    // if there is room, ask user for what to add
-    } else{
-        printf("\nEnter elements to add: ");
-        scanf("%d", &x);
-        // increment top to add 1 for the element added
-        top = top + 1;
-        //store new element in array loop indexed by top
-        inp_array[top] = x;
+        // Validate range of choice
+        if (choice < 1 || choice > 4) {
+            printf("\nInvalid choice! Please select a number between 1 and 4.");
+            continue;
+        }
+
+        // Cases for all functions
+        switch (choice) {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            show();
+            break;
+        case 4:
+            printf("\nExiting program. Goodbye!\n");
+            exit(0);
+        default:
+            printf("\nInvalid input.");
+        }
     }
 }
-// push
-// pop
-// top: returns top element of the stack
-// isEmpty:returns true if stack is empty else false
-// isFull: returns true if the stack is full else false
-    // if(top == capacity-1) stack overflows
-    // else (top = top+1)
+
+// Push operation
+void push() {
+    int x;
+
+    if (top == SIZE - 1) {
+        printf("\nOverflow! Stack is full.");
+    } else {
+        printf("\nEnter element to add: ");
+        if (scanf("%d", &x) != 1) {
+            printf("\nInvalid input! Please enter a number.");
+            while (getchar() != '\n'); // Clear buffer
+            return;
+        }
+        top = top + 1; // Increment top
+        inp_array[top] = x; // Add element to stack
+        printf("\nElement %d added to stack.", x);
+    }
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) { // Check for underflow
+        printf("\nUnderflow! Stack is empty.");
+    } else {
+        printf("\nPopped element: %d", inp_array[top]);
+        top = top - 1; // Decrement top
+    }
+}
+
+// Show operation
+void show() {
+    if (top == -1) { // Check for empty stack
+        printf("\nUnderflow! Stack is empty.");
+    } else {
+        printf("\nElements in the stack (top to bottom):");
+        for (int i = top; i >= 0; --i) {
+            printf("\nElement: %d", inp_array[i]);
+        }
+    }
+}
